10b6d9b8af4a439d164b9362b9d3fd0d2425e93a
Kartikeya Singh <105882265+karkau123@users.noreply.github.com>
2024-01-05 02:34:26 +0530

    background created

---
+++ lib/homepage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Column(
+        children: [
+         Expanded(flex: 2,child: Container(color: Colors.blue,),),
+         Expanded(child: Container(color: Colors.green,))
+        
+
+        ],
+      ),
+    );
+  }
+}
